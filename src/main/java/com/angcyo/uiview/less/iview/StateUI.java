01cb2a4879e8bc050c3def4cf2feb74f581c7e4a
angcyo <angcyo@126.com>
2018-11-13 00:38:50 +0800

    fix state ui

--- src/main/java/com/angcyo/uiview/less/iview/StateUI.java
+++ src/main/java/com/angcyo/uiview/less/iview/StateUI.java
@@ -3,6 +3,7 @@ package com.angcyo.uiview.less.iview;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
+import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.view.LayoutInflater;
@@ -11,11 +12,14 @@ import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.FrameLayout;
-import android.widget.TextView;
 import com.angcyo.uiview.less.R;
+import com.angcyo.uiview.less.recycler.RBaseViewHolder;
 import com.angcyo.uiview.less.resources.AnimUtil;
 
 /**
+ * 可以在Activity 中, 弹出自定义布局, 并且控制
+ * <p>
+ * <p>
  * Email:angcyo@126.com
  *
  * @author angcyo
@@ -30,7 +34,6 @@ public class StateUI {
 
     ViewGroup parent;
     Context context;
-    String tip = "请稍等...";
     boolean canCancelOnTouchOutside = false;
 
     Animation startAnimation = defaultStartAnimation;
@@ -39,20 +42,40 @@ public class StateUI {
     int bgStartColor = DEFAULT_START_COLOR;
     int bgEndColor = DEFAULT_END_COLOR;
 
-    public StateUI(@NonNull ViewGroup parent) {
+    int layoutId;
+
+    OnStateInitListener listener;
+
+    public RBaseViewHolder baseViewHolder;
+
+    public StateUI(@NonNull ViewGroup parent, @LayoutRes int layoutId) {
         this.parent = parent;
+        this.layoutId = layoutId;
         context = parent.getContext();
     }
 
-    public static StateUI flow(@NonNull Activity activity) {
+    public static StateUI custom(@NonNull Activity activity, @LayoutRes int layoutId) {
         View decorView = activity.getWindow().getDecorView();
+        View viewWithTag = decorView.findViewWithTag(layoutId);
+        if (viewWithTag != null) {
+            return (StateUI) viewWithTag.getTag(R.id.tag_state_ui);
+        }
+        return new StateUI((ViewGroup) decorView, layoutId);
+    }
+
+    /**
+     * 请调用 {@link #show()} 方法, 才能显示
+     */
+    public static StateUI flow(@NonNull Activity activity) {
+//        View decorView = activity.getWindow().getDecorView();
         //View contentView = activity.getWindow().findViewById(Window.ID_ANDROID_CONTENT);
 
         //View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
         //int statusBarHeight = contentView.getTop();//小米8se 110
         //int navigationBarHeight = decorView.getMeasuredHeight() - contentView.getMeasuredHeight();//小米8se 130
 
-        return new StateUI((ViewGroup) decorView);
+//        return new StateUI((ViewGroup) decorView);
+        return custom(activity, R.layout.base_flow_loading_layout);
     }
 
     /**
@@ -61,39 +84,37 @@ public class StateUI {
     public static boolean hide(@NonNull Activity activity) {
         final View decorView = activity.getWindow().getDecorView();
         if (decorView instanceof ViewGroup) {
-            final View view = decorView.findViewWithTag(R.layout.base_flow_loading_layout);
-            if (view instanceof ViewGroup) {
-                Animation finishAnimation = defaultFinishAnimation;
-                finishAnimation.setAnimationListener(new Animation.AnimationListener() {
-                    @Override
-                    public void onAnimationStart(Animation animation) {
-
-                    }
-
-                    @Override
-                    public void onAnimationEnd(Animation animation) {
-                        ((ViewGroup) decorView).removeView(view);
-                    }
-
-                    @Override
-                    public void onAnimationRepeat(Animation animation) {
-
-                    }
-                });
-                AnimUtil.startArgb(view, DEFAULT_END_COLOR, DEFAULT_START_COLOR, 300);
-                ((ViewGroup) view).getChildAt(0).startAnimation(finishAnimation);
-
-                return false;
+            //一层一层隐藏
+            int childCount = ((ViewGroup) decorView).getChildCount();
+            for (int i = childCount - 1; i >= 0; i--) {
+                View childAt = ((ViewGroup) decorView).getChildAt(i);
+                if (childAt instanceof StateLayout) {
+                    StateUI stateUI = (StateUI) childAt.getTag(R.id.tag_state_ui);
+                    stateUI.hide();
+                    return false;
+                }
             }
+            return true;
         }
-
         return true;
     }
 
-    public StateUI setTipText(String tip) {
-        this.tip = tip;
-        initLayout();
-        return this;
+    /**
+     * 是否还有布局在显示
+     */
+    public static boolean haveStateUI(@NonNull Activity activity) {
+        final View decorView = activity.getWindow().getDecorView();
+        if (decorView instanceof ViewGroup) {
+            int childCount = ((ViewGroup) decorView).getChildCount();
+            for (int i = childCount - 1; i >= 0; i--) {
+                View childAt = ((ViewGroup) decorView).getChildAt(i);
+                if (childAt instanceof StateLayout) {
+                    return true;
+                }
+            }
+            return false;
+        }
+        return false;
     }
 
     private void hideSoftInputFromWindow() {
@@ -101,29 +122,26 @@ public class StateUI {
         manager.hideSoftInputFromWindow(parent.getWindowToken(), 0);
     }
 
-    private boolean initLayout() {
-        if (parent == null) {
-            return false;
-        }
-
-        final View view = parent.findViewWithTag(R.layout.base_flow_loading_layout);
-        if (view != null) {
-            ((TextView) view.findViewById(R.id.base_load_tip_view)).setText(tip);
-            return true;
-        }
-        return false;
+    public StateUI setInitListener(OnStateInitListener listener) {
+        this.listener = listener;
+        return this;
     }
 
     public StateUI show() {
-        if (initLayout()) {
+        if (baseViewHolder != null) {
             return this;
         }
 
         hideSoftInputFromWindow();
 
         LayoutInflater inflater = LayoutInflater.from(context);
-        FrameLayout overlay = new FrameLayout(context);
-        overlay.setTag(R.layout.base_flow_loading_layout);
+
+        //套一层
+        StateLayout overlay = new StateLayout(context);
+        baseViewHolder = new RBaseViewHolder(overlay);
+        overlay.setTag(R.id.tag_state_ui, this);
+
+        overlay.setTag(layoutId);
         overlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -134,10 +152,11 @@ public class StateUI {
         });
         AnimUtil.startArgb(overlay, bgStartColor, bgEndColor, 300);
 
-        inflater.inflate(R.layout.base_flow_loading_layout, overlay);
-        ((TextView) overlay.findViewById(R.id.base_load_tip_view)).setText(tip);
+        inflater.inflate(layoutId, overlay);
 
-        overlay.getChildAt(0).startAnimation(startAnimation);
+        if (listener != null) {
+            listener.onStateInit(this);
+        }
 
         if (parent instanceof FrameLayout) {
             parent.addView(overlay, new FrameLayout.LayoutParams(-1, -1));
@@ -145,11 +164,15 @@ public class StateUI {
             parent.addView(overlay, new ConstraintLayout.LayoutParams(-1, -1));
         }
 
+        if (startAnimation != null) {
+            overlay.getChildAt(0).startAnimation(startAnimation);
+        }
+
         return this;
     }
 
     public void hide() {
-        final View view = parent.findViewWithTag(R.layout.base_flow_loading_layout);
+        final View view = parent.findViewWithTag(layoutId);
         if (view instanceof ViewGroup) {
             if (finishAnimation == null) {
                 parent.removeView(view);
@@ -176,4 +199,14 @@ public class StateUI {
         }
     }
 
+    public interface OnStateInitListener {
+        void onStateInit(StateUI stateUI);
+    }
+
+    private static class StateLayout extends FrameLayout {
+
+        public StateLayout(@NonNull Context context) {
+            super(context);
+        }
+    }
 }
