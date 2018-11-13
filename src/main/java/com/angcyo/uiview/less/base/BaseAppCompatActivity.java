687037a0b7404d4bca17ecac8ff50bffa3cac458
angcyo <angcyo@126.com>
2018-11-13 11:05:56 +0800

    add base app compat

---
+++ src/main/java/com/angcyo/uiview/less/base/BaseAppCompatActivity.java
@@ -0,0 +1,23 @@
+package com.angcyo.uiview.less.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import com.angcyo.uiview.less.recycler.RBaseViewHolder;
+
+/**
+ * Email:angcyo@126.com
+ *
+ * @author angcyo
+ * @date 2018/11/13
+ */
+public class BaseAppCompatActivity extends AppCompatActivity {
+
+    protected RBaseViewHolder viewHolder;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        viewHolder = new RBaseViewHolder(getWindow().getDecorView());
+    }
+}
