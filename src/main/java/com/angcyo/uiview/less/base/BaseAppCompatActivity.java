737e2d12de24a659559a20e013996e60ad96ebf8
angcyo <angcyo@126.com>
2018-11-15 09:11:03 +0800

    fix base

--- src/main/java/com/angcyo/uiview/less/base/BaseAppCompatActivity.java
+++ src/main/java/com/angcyo/uiview/less/base/BaseAppCompatActivity.java
@@ -1,6 +1,11 @@
 package com.angcyo.uiview.less.base;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import com.angcyo.lib.L;
@@ -22,5 +27,26 @@ public class BaseAppCompatActivity extends AppCompatActivity {
         viewHolder = new RBaseViewHolder(getWindow().getDecorView());
 
         L.v("taskId:" + getTaskId());
+    }
+
+    /**
+     * 请求忽略电池优化, 在后台存活机会会加大
+     */
+    public void ignoreBatteryOptimization() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            String packageName = getPackageName();
+            PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+            if (!pm.isIgnoringBatteryOptimizations(packageName)) {
+                try {
+                    //some device doesn't has activity to handle this intent
+                    //so add try catch
+                    Intent intent = new Intent();
+                    intent.setAction(android.provider.Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+                    intent.setData(Uri.parse("package:" + packageName));
+                    startActivity(intent);
+                } catch (Exception e) {
+                }
+            }
+        }
     }
 }
