b57817b98a7f1f7e604a69152b14fc10f1df133f
angcyo <angcyo@126.com>
2018-11-20 23:18:42 +0800

    add listener

---
+++ src/main/java/com/angcyo/uiview/less/resources/RAnimtionListener.kt
@@ -0,0 +1,25 @@
+package com.angcyo.uiview.less.resources
+
+import android.view.animation.Animation
+
+/**
+ * Copyright (C) 2016,深圳市红鸟网络科技股份有限公司 All rights reserved.
+ * 项目名称：
+ * 类的描述：
+ * 创建人员：Robi
+ * 创建时间：2017/07/21 14:42
+ * 修改人员：Robi
+ * 修改时间：2017/07/21 14:42
+ * 修改备注：
+ * Version: 1.0.0
+ */
+abstract class RAnimtionListener : Animation.AnimationListener {
+    override fun onAnimationRepeat(animation: Animation?) {
+    }
+
+    override fun onAnimationEnd(animation: Animation?) {
+    }
+
+    override fun onAnimationStart(animation: Animation?) {
+    }
+}
