c9a316cc62f6c0068678ffb3650d4eb0dbc5d769
angcyo <angcyo@126.com>
2018-12-02 15:43:12 +0800

    fix base

--- src/main/java/com/angcyo/uiview/less/base/BaseAppCompatActivity.java
+++ src/main/java/com/angcyo/uiview/less/base/BaseAppCompatActivity.java
@@ -1,27 +1,16 @@
 package com.angcyo.uiview.less.base;
 
 import android.Manifest;
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.Resources;
-import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.PowerManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Gravity;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
-import android.widget.FrameLayout;
-import android.widget.TextView;
 import com.angcyo.lib.L;
-import com.angcyo.uiview.less.BuildConfig;
 import com.angcyo.uiview.less.recycler.RBaseViewHolder;
-import com.angcyo.uiview.less.utils.ScreenUtil;
 import com.tbruyelle.rxpermissions.Permission;
 import com.tbruyelle.rxpermissions.RxPermissions;
 import rx.functions.Action1;
@@ -140,10 +129,10 @@ public class BaseAppCompatActivity extends AppCompatActivity {
             @Override
             public void call(String s) {
                 if (s.contains("0")) {
-                    //有权限被拒绝
+                    /*有权限被拒绝*/
                     onResult.call(false);
                 } else {
-                    //所欲权限通过
+                    /*所欲权限通过*/
                     onResult.call(true);
                 }
             }
@@ -186,46 +175,7 @@ public class BaseAppCompatActivity extends AppCompatActivity {
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
         if (hasFocus) {
-            addDebugTextView(this);
-        }
-    }
-
-    public static void addDebugTextView(Activity activity) {
-        if (BuildConfig.DEBUG) {
-            //添加一个TextView,用来提示当前的Activity类
-            View decorView = activity.getWindow().getDecorView();
-            View rootView = activity.getWindow().findViewById(Window.ID_ANDROID_CONTENT);
-            final String tag = "addDebugTextView";
-            View debugTextView = decorView.findViewWithTag(tag);
-            if (debugTextView != null) {
-                return;
-            }
-            if (decorView instanceof FrameLayout) {
-                TextView textView = new TextView(activity);
-                textView.setTag(tag);
-                textView.setTextSize(9);
-                textView.setTextColor(Color.WHITE);
-                float dp2 = 1 * ScreenUtil.density();
-                int padding = (int) dp2 * 4;
-                textView.setPadding(padding, padding, padding, padding);
-                textView.setShadowLayer(dp2 * 2, dp2, dp2, Color.BLACK);
-
-                textView.setText(activity.getClass().getSimpleName());
-
-                FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(-2, -2);
-                layoutParams.gravity = Gravity.BOTTOM;
-                if (decorView.getBottom() > rootView.getBottom()) {
-                    //显示了导航栏
-                    Resources resources = activity.getResources();
-                    int resourceId = resources.getIdentifier("navigation_bar_height", "dimen", "android");
-                    int navBarHeight = 0;
-                    if (resourceId > 0) {
-                        navBarHeight = resources.getDimensionPixelSize(resourceId);
-                    }
-                    layoutParams.bottomMargin = navBarHeight;
-                }
-                ((ViewGroup) decorView).addView(textView, layoutParams);
-            }
+            Debug.addDebugTextView(this);
         }
     }
 }
---
+++ src/main/java/com/angcyo/uiview/less/base/Debug.java
@@ -0,0 +1,57 @@
+package com.angcyo.uiview.less.base;
+
+import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import com.angcyo.uiview.less.BuildConfig;
+import com.angcyo.uiview.less.utils.ScreenUtil;
+
+/**
+ * Created by angcyo on 2018/12/02 15:38
+ */
+public class Debug {
+    public static void addDebugTextView(Activity activity) {
+        if (BuildConfig.DEBUG) {
+            //添加一个TextView,用来提示当前的Activity类
+            View decorView = activity.getWindow().getDecorView();
+            View rootView = activity.getWindow().findViewById(Window.ID_ANDROID_CONTENT);
+            final String tag = "addDebugTextView";
+            View debugTextView = decorView.findViewWithTag(tag);
+            if (debugTextView != null) {
+                return;
+            }
+            if (decorView instanceof FrameLayout) {
+                TextView textView = new TextView(activity);
+                textView.setTag(tag);
+                textView.setTextSize(9);
+                textView.setTextColor(Color.WHITE);
+                float dp2 = 1 * ScreenUtil.density();
+                int padding = (int) dp2 * 4;
+                textView.setPadding(padding, padding, padding, padding);
+                textView.setShadowLayer(dp2 * 2, dp2, dp2, Color.BLACK);
+
+                textView.setText(activity.getClass().getSimpleName());
+
+                FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(-2, -2);
+                layoutParams.gravity = Gravity.BOTTOM;
+                if (decorView.getBottom() > rootView.getBottom()) {
+                    //显示了导航栏
+                    Resources resources = activity.getResources();
+                    int resourceId = resources.getIdentifier("navigation_bar_height", "dimen", "android");
+                    int navBarHeight = 0;
+                    if (resourceId > 0) {
+                        navBarHeight = resources.getDimensionPixelSize(resourceId);
+                    }
+                    layoutParams.bottomMargin = navBarHeight;
+                }
+                ((ViewGroup) decorView).addView(textView, layoutParams);
+            }
+        }
+    }
+}
